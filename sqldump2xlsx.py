d909e724688bcb08d77ed11ca3f159413594b266
Markus Thilo <markus.thilo@gmail.com>
2022-02-19 16:03:36 +0100

    Still fixing bugs on the cmd version

--- sqldump2xlsx.py
+++ sqldump2xlsx.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Markus Thilo'
-__version__ = '0.3_2022-02-18'
+__version__ = '0.3_2022-02-19'
 __license__ = 'GPL-3'
 __email__ = 'markus.thilo@gmail.com'
 __status__ = 'Testing'
